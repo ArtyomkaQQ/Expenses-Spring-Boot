fe2697bf44421bd5099efd4e3ed228f2708512c0
Artyom Aralov <artyom.aralov@gmail.com>
2020-04-10 17:46:05 +0300

    Code ref

--- src/main/java/com/example/dev/expense/ExpenseApplication.java
+++ src/main/java/com/example/dev/expense/ExpenseApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -26,9 +25,7 @@ public class ExpenseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Set<Expense> expenses = Set.of(new Expense(1L, "Lenovo laptop",  Category.ELECTRONICS, LocalDateTime.now(), "Tallinn")
-        );
-
+        Set<Expense> expenses = Set.of(new Expense(1L, "Lenovo laptop",  Category.ELECTRONICS, LocalDateTime.now(), "Tallinn"));
         expenseRepository.saveAll(expenses);
     }
 
--- src/main/java/com/example/dev/expense/model/Category.java
+++ src/main/java/com/example/dev/expense/model/Category.java
@@ -3,8 +3,10 @@ package com.example.dev.expense.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum Category {
+
     @JsonProperty("Food")
     FOOD,
     @JsonProperty("Electronics")
     ELECTRONICS
+
 }
